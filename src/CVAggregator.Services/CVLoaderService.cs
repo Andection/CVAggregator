055505424167e185baaf7aadc7a4ffb6a3301356
andection <andection@gmail.com>
2015-05-01 11:24:34 +0400

    [*] load items

--- src/CVAggregator.Services/CVLoaderService.cs
+++ src/CVAggregator.Services/CVLoaderService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AggregatorService.Domain;
+using HtmlAgilityPack;
 
 namespace CVAggregator.Services
 {
@@ -13,9 +15,28 @@ namespace CVAggregator.Services
             _rootUri = rootUri;
         }
 
-        public Task<CurriculumVitae[]> LoadCurriculumVitae(int pageIndex, int pageSize)
+        public async Task<CurriculumVitae[]> LoadCurriculumVitae(int pageIndex, int pageSize)
         {
-            throw new NotImplementedException();
+            using (var httpClient = new HttpClient())
+            {
+                var rawHtml = await httpClient.GetStringAsync(string.Format("{0}?limit={1}&offset={2}", _rootUri, pageSize, pageSize*pageIndex));
+
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(rawHtml);
+
+                var allContent = htmlDocument.GetElementbyId("ra-content");
+                return allContent.SelectSingleNode("//div[@class='ra-elements-container']")
+                                 .SelectSingleNode("//ul[@class='ra-elements-list-hidden']")
+                                 .Descendants("li")
+                                 .Select(Map)
+                                 .ToArray();
+
+            }
+        }
+
+        private static CurriculumVitae Map(HtmlNode item)
+        {
+            return new CurriculumVitae();
         }
     }
 }
