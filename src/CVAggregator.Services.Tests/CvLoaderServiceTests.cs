9a9edf42e40dd8198c5a6689bc27815f3b484ade
andection <andection@gmail.com>
2015-05-01 10:22:19 +0400

    add test to resume page loading

---
+++ src/CVAggregator.Services.Tests/CvLoaderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CVAggregator.Services.Tests
+{
+    [TestFixture]
+    public class CvLoaderServiceTests
+    {
+        private CvLoaderService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new CvLoaderService("http://rabota.e1.ru/resume");
+        }
+
+        [Test]
+        public async void should_load_page_of_resume()
+        {
+            const int pageSize = 100;
+            var page = await _service.LoadCurriculumVitae(0, pageSize);
+
+            page.Should().NotBeNull();
+            page.Should().HaveCount(pageSize);
+        }
+    }
+}
---
+++ src/CVAggregator.Services/CVLoaderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using AggregatorService.Domain;
+
+namespace CVAggregator.Services
+{
+    public class CvLoaderService
+    {
+        private readonly string _rootUri;
+
+        public CvLoaderService(string rootUri)
+        {
+            _rootUri = rootUri;
+        }
+
+        public Task<CurriculumVitae[]> LoadCurriculumVitae(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
